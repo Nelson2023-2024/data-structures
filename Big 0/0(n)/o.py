c26a8ff3022a0b3e619d7edde2522c0241ddb75c
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-01-14 15:33:06 +0300

    Linear time

---
+++ Big 0/0(n)/o.py
@@ -0,0 +1,35 @@
+"""
+ Big O: O(n)
+Implement a Python function called print_items.
+
+This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+
+The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+
+The function signature should be: def print_items(n):
+
+
+Example:
+
+Here is an example of how your function should behave:
+
+
+If you call print_items(10), your function should print:
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+"""
+
+def print_items(integer):
+    for i in range(integer): # the time complexity increases linearly based on the input size 0(n)
+        print(i)            # the time complexity here is 0(1) as the computation always remains the same
+
+print_items(10)
