620a7f996cdd51eed0ac337dc71fe620132704b1
Nelson2023-2024 <nelsonobuya19@gmail.com>
2026-01-22 05:50:43 +0300

    Bubble sort implemented

---
+++ Sorting-Algorithms/Bubble_Sort/test.py
@@ -0,0 +1,12 @@
+def bubble(list):
+    for i in range(len(list) -1, 0, -1):
+        print(i)
+        for j in range(i):
+            if list[j] > list[j + 1]:
+                temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+    return list
+
+
+print(bubble([18,62,93,4,56,7]))
